0618dc455197462bcc59321ec30c019f891c6bfe
Francisco José Lavalle <franlavalle@hotmail.com>
2022-09-27 20:11:53 -0300

    Subo archivos 2

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors';
 import mongoose from "mongoose";
 
 const connection = mongoose.connect("mongodb+srv://fran:1234@rifas.hax0xip.mongodb.net/?retryWrites=true&w=majority")
@@ -7,6 +8,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8080
 const server = app.listen(PORT,() => console.log(`Listening on ${PORT}`))
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/',(req,res) => {
